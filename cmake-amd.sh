8f203582472a148ce9b70036c7f4a589b983da02
eugeneswalker <eugenesunsetwalker@gmail.com>
2022-01-25 16:02:07 -0800

    add cmake for amd gpu build

---
+++ cmake-amd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+spack load cmake
+spack load binutils
+spack load hwloc
+spack load libedit
+spack load ncurses
+spack load swig
+spack load libelf
+
+cmake \
+ -DCMAKE_INSTALL_PREFIX=/llvm-install-gpu-0 \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DLLVM_ENABLE_PROJECTS=clang \
+ -DLLVM_ENABLE_RUNTIMES=openmp \
+ -DLLVM_TARGETS_TO_BUILD="host;AMDGPU" \
+ -DCMAKE_C_COMPILER=gcc \
+ -DCMAKE_CXX_COMPILER=g++ \
+ /llvm-project/llvm
